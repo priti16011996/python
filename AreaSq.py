383333ce550fda33a2e5db490325108f5186eed1
Priti <priti16maurya@gmail.com>
2026-05-08 22:24:09 +0530

    Learning Python from Scratch

---
+++ AreaSq.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the side of Square: "))
+area = a*a;
+print("Area of Square: ", area);
---
+++ avg.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter First Number: "));
+num2 = int(input("Enter Second Number: "));
+avg = (num1+num2)/2;
+print("Average of number :", avg);
